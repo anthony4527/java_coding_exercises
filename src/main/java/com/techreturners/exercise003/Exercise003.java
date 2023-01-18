2a58c5f1ae1089879f0e8b8c74cdff09bb47e84b
AnthonyLee <anthony@calglobal.com>
2023-01-19 05:19:31 +0800

    finish exercise 3

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,23 @@
+
 package com.techreturners.exercise003;
 
 public class Exercise003 {
 
+    // set up Array for icecream codes
+    String[] icStore = {"Pistachio","Raspberry Ripple","Vanilla", "Mint Chocolate Chip","Chocolate","Mango Sorbet"};
+
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        for (int i=0; i < icStore.length; i++){
+            if (icStore[i] == iceCreamFlavour )
+                return i;
+         }
+        throw new UnsupportedOperationException("Your favour is not in store");
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return icStore;
+        //this method returns all flavour in the store and no need to throw error
+        // throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
     }
 
 }
